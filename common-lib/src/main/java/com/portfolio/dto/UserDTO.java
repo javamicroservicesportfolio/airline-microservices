b3840a2ca13c97055313859b9d70e3d29a828f48
Keith <karirukeith@gmail.com>
2026-07-21 00:37:23 +0300

    Added some changes related to properties and mapping

--- common-lib/src/main/java/com/portfolio/dto/UserDTO.java
+++ common-lib/src/main/java/com/portfolio/dto/UserDTO.java
@@ -5,18 +5,16 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserDTO {
-    private Long id;
     private String email;
     private String password;
-    private String fullName;
+    private String firstName;
+    private String lastName;
     private String phone;
     private UserRole role;
-    private LocalDateTime lastLogin;
 }
